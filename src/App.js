0a864003a911c4920c518dbbef57f2cb7b319416
OmarGC <omar.95gc@gmail.com>
2019-09-29 19:14:15 -0500

    Ejemplo basico del uso de useEffect con axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+// Se puede aplicar destructuring: {phrase}, y se quita props
+const Phrase = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frase">
+      <h1> {props.phrase.quote} </h1>
+      <p> - {props.phrase.author} </p>
     </div>
   );
 }
+
+const App = () => {
+  // phrase = frase && quotes = frases
+  const [phrase, setPhrase] = useState({});
+
+  // Metodo para consultar datos de la API
+  const getApi = async () => {
+    const res = await axios(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+
+    // console.log(res.data[0]);
+    // Agregar el resultado de la API al state, (similar a this.setState)
+    setPhrase(res.data[0]);
+  };
+
+  // consulta a rest API
+  useEffect(
+    () => {
+      getApi();
+    },
+    [] // Que se tiene que actualizar para que el consultar api se llame de nuevo
+  );
+
+  // phrase = this.state
+  // console.log(phrase);
+
+  return (
+    <div className="contenedor">
+      <Phrase
+        phrase = { phrase }
+      />
+      <button
+        onClick={getApi}
+      >
+        Generar Nueva Frase
+      </button>
+    </div>
+  );
+};
 
 export default App;
